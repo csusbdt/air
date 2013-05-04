42302ad84a99dd4d897cfbc7bd6dd8f40564d46e
David Turner <csusbdt@gmail.com>
2013-05-04 11:06:12 -0700

    refactored web server test

--- web/main.js
+++ web/main.js
@@ -1,5 +1,8 @@
 var connect = require('connect');
 var port = process.env.PORT;
 if (port === undefined) port = 5000;
-connect.createServer(connect.static('web/public')).listen(port);
+connect.createServer(connect.static('public')).listen(port, function(err) {
+  if (err) console.log(err.message);
+  else console.log('Listenning on port 5000.');
+});
 
